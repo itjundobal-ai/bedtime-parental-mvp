61dc3fcc0ced99c0f46c9bb9318b6ad27b3321dc
itjundobal-ai <itjundobal@gmail.com>
2026-09-01 16:54:44 +0800

    Add Child app Gradle config

---
+++ android/childapp/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.master.bedtime.child"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.master.bedtime.child"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
