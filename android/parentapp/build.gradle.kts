a639c471d788cee5792848174dac24b0fbecdff6
itjundobal-ai <itjundobal@gmail.com>
2026-09-01 16:54:36 +0800

    Add Parent app Gradle config

---
+++ android/parentapp/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.master.bedtime.parent"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.master.bedtime.parent"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
